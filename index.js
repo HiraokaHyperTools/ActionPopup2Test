3cc4c28e8efdca50f19cd8f34c0cff26f23afc56
kenjiuno <ku@digitaldolphins.jp>
2022-01-18 13:39:18 +0900

    0.1

---
+++ index.js
@@ -0,0 +1,26 @@
+const archiver = require('archiver');
+const fs = require('fs');
+
+// https://stackoverflow.com/a/51518100
+
+function zipDirectory(sourceDir, outPath) {
+    return new Promise((resolve, reject) => {
+        const archive = archiver('zip');
+        const stream = fs.createWriteStream(outPath);
+
+        stream.on('error', ex => reject(ex));
+        stream.on('close', () => resolve());
+
+        archive.directory(sourceDir, false);
+        archive.on('error', err => reject(err));
+        archive.pipe(stream);
+
+        archive.finalize();
+    });
+}
+
+zipDirectory("action_popup", "action_popup.xpi")
+    .then(
+        () => console.info("action_popup.xpi done."),
+        ex => console.error("FAILED! " + ex)
+    );
